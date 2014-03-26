a5d1488a582149beef51da89d2c58dcd9c2a9ff9
Max Edmands <max@goodeggs.com>
2014-03-26 09:43:34 -0700

    Warning note about xcode before asking for your password

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # some tools will complain about not knowing where binaries are, until you do this:
+echo "Making sure you have XCode command line tools..."
 sudo xcode-select --install
 
 # set up a basic .profile
