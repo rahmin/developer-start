0f1ca8938dd084f52eba8c874cab225e247f0c35
Aaron Borden <aaron@goodeggs.com>
2014-06-18 14:00:43 -0700

    Don't overwrite .sekret

--- setup.sh
+++ setup.sh
@@ -101,14 +101,16 @@ launchctl load ~/Library/LaunchAgents/homebrew.mxcl.rabbitmq.plist
 brew install phantomjs selenium-server-standalone chromedriver
 
 # get production mongodb credentials
-read -t 0.1 -n 10000 discard # flush input from stdin
-read -p "Please enter the AWS access key ID for mongolabs from our 1password vault: " -e -s AWS_ACCESS_KEY_ID
-read -p "And what is the secret access key? " -e -s AWS_SECRET_ACCESS_KEY
-cat <<EOF >> ~/.sekret
+if [[ ! -f ~/.sekret ]]; then
+    read -t 0.1 -n 10000 discard # flush input from stdin
+    read -p "Please enter the AWS access key ID for mongolabs from our 1password vault: " -e -s AWS_ACCESS_KEY_ID
+    read -p "And what is the secret access key? " -e -s AWS_SECRET_ACCESS_KEY
+    cat <<EOF > ~/.sekret
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
 EOF
+fi
 
 # install dump-and-restore
 npm install -g dump-and-restore
